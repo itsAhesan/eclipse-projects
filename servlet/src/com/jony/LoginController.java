6768f9aede298f7e6c70dfa2d390fd669a3f09d0
Ahesan Chowdhury <ahesanchowdhury2016@gmail.com>
2024-07-22 23:42:15 +0530

    Add files via upload

---
+++ servlet/src/com/jony/LoginController.java
@@ -0,0 +1,47 @@
+package com.jony;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class LoginController implements Servlet{
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ServletConfig getServletConfig() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getServletInfo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void init(ServletConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		res.setContentType("text/html");
+		PrintWriter writer = res.getWriter();
+		writer.print("<h1 align='center' >Hello guyss.....</h1>");
+		
+	}
+	
+
+}

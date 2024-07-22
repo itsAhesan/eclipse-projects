ae255344c756fca0551b53cfb5f12da02d2d1a26
Ahesan Chowdhury <ahesanchowdhury2016@gmail.com>
2024-07-22 23:35:01 +0530

    Add files via upload

---
+++ e-com app/src/com/jony/ClothsServlet.java
@@ -0,0 +1,34 @@
+package com.jony;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ClothsServlet extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter writer = resp.getWriter();
+		
+		ServletContext context = getServletContext();
+		String offer = context.getInitParameter("offer");
+		
+		writer.print(offer);
+		
+		ServletConfig config = getServletConfig();
+		String discount = config.getInitParameter("discount");
+		
+		writer.print("<br>Cloths discount "+discount);
+		
+		
+
+	}
+	
+
+}
